a693dc54096a1b6a174b4838eba90ee50b0e2088
Jonathan Swartz <jonathan@jswartz.info>
2025-09-23 20:34:37 -0400

    Fix spaces not tabs
    Signed-off-by: Jonathan Swartz <jonathan@jswartz.info>

--- dev_utils/run_format.sh
+++ dev_utils/run_format.sh
@@ -5,12 +5,12 @@
 
 # Find all relevant source files, excluding build directories, and format them with clang-format
 find . -type f \
-	\( -name "*.c" \
-	-o -name "*.cpp" \
-	-o -name "*.h" \
-	-o -name "*.hpp" \) \
+    \( -name "*.c" \
+    -o -name "*.cpp" \
+    -o -name "*.h" \
+    -o -name "*.hpp" \) \
     -not -path "*/build/*" \
-	-print0 | xargs -0 clang-format -i --verbose
+    -print0 | xargs -0 clang-format -i --verbose
 
 echo "Running black on Python files..."
 python -m black ./pymodule ./pytests --verbose --target-version=py311 --line-length=120
