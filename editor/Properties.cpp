38a2578f88aca5af4c79bc649cc5d91c08d14801
Petra Hapalova <phapalova@nvidia.com>
2025-10-20 23:37:16 +0200

    fix whitespace
    Signed-off-by: Petra Hapalova <phapalova@nvidia.com>

--- editor/Properties.cpp
+++ editor/Properties.cpp
@@ -366,7 +366,7 @@ void Properties::render(imgui_instance_user::Instance* ptr)
 {
     if (ImGui::Begin(PROPERTIES, &ptr->window.show_scene_properties))
     {
-        
+
         auto selection = ptr->editor_scene->get_properties_selection();
         if (selection.name.empty() || selection.type == pnanovdb_editor::ViewType::None)
         {
