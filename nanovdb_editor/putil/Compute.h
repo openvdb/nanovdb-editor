7a7576b5379f93edabbde7c4c933dfd646e0e40b
Andrew Reidmeyer <areidmeyer@nvidia.com>
2025-11-12 23:57:48 -0600

    Function to embed image in NanoVDB.
    Signed-off-by: Andrew Reidmeyer <areidmeyer@nvidia.com>

--- nanovdb_editor/putil/Compute.h
+++ nanovdb_editor/putil/Compute.h
@@ -842,6 +842,9 @@ typedef struct pnanovdb_compute_t
                                                   const char* name,
                                                   pnanovdb_compute_array_t* arr,
                                                   pnanovdb_uint32_t channel_count);
+    pnanovdb_compute_array_t*(PNANOVDB_ABI* nanovdb_from_image_rgba8)(pnanovdb_compute_array_t* image_data, 
+                                                                      pnanovdb_uint32_t width, 
+                                                                      pnanovdb_uint32_t height);
 } pnanovdb_compute_t;
 
 #define PNANOVDB_REFLECT_TYPE pnanovdb_compute_t
@@ -864,6 +867,7 @@ PNANOVDB_REFLECT_FUNCTION_POINTER(destroy_array, 0, 0)
 PNANOVDB_REFLECT_FUNCTION_POINTER(map_array, 0, 0)
 PNANOVDB_REFLECT_FUNCTION_POINTER(unmap_array, 0, 0)
 PNANOVDB_REFLECT_FUNCTION_POINTER(compute_array_print_range, 0, 0)
+PNANOVDB_REFLECT_FUNCTION_POINTER(nanovdb_from_image_rgba8, 0, 0)
 PNANOVDB_REFLECT_END(0)
 PNANOVDB_REFLECT_INTERFACE_IMPL()
 #undef PNANOVDB_REFLECT_TYPE
