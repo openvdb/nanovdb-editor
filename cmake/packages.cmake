3fa99ef1b689430d30240d6d14262a9f92af2594
Petra Hapalova <phapalova@nvidia.com>
2026-01-08 12:18:16 +0100

    fix slang-llvm install path
    Signed-off-by: Petra Hapalova <phapalova@nvidia.com>

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -278,6 +278,8 @@ if(NANOVDB_EDITOR_BUILD_SLANG_FROM_SOURCE)
     include(ExternalProject)
     set(SLANG_INSTALL_DIR "${CMAKE_BINARY_DIR}/slang-install")
     set(SLANG_INSTALLED_LIB "${SLANG_INSTALL_DIR}/lib/libslang${CMAKE_SHARED_LIBRARY_SUFFIX}")
+    # slang-llvm is downloaded by Slang's build to a version-specific subdirectory
+    set(SLANG_BUILD_DIR "${CMAKE_BINARY_DIR}/slang_src_build-prefix/src/slang_src_build-build")
 
     ExternalProject_Add(slang_src_build
         GIT_REPOSITORY https://github.com/shader-slang/slang.git
