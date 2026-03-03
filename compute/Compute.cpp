0a6e15461aad89b1b8b80ad885bcecdd9b7a8eba
Petra Hapalova <phapalova@nvidia.com>
2026-03-03 09:30:28 +0100

    fix bulid and version bump
    Signed-off-by: Petra Hapalova <phapalova@nvidia.com>

--- compute/Compute.cpp
+++ compute/Compute.cpp
@@ -325,8 +325,8 @@ pnanovdb_bool_t dispatch_shader_on_nanovdb_array(const pnanovdb_compute_t* compu
     resources[3u].buffer_transient = upload_buffer;
     resources[4u].buffer_transient = user_upload_buffer;
 
-    compute->dispatch_shader(compute_interface, compute_context, shader_context, resources.data(),
-                             (image_width + 31u) / 32u, (image_height + 3u) / 4u, 1u, "dispatch_shader_on_nanovdb_array");
+    compute->dispatch_shader(compute_interface, compute_context, shader_context, resources, (image_width + 31u) / 32u,
+                             (image_height + 3u) / 4u, 1u, "dispatch_shader_on_nanovdb_array");
 
     if (*readback_buffer)
     {
