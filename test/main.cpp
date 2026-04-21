2d51fbc5d62747bd9c1aeba02b9aae05ca820711
Andrew Reidmeyer <areidmeyer@nvidia.com>
2026-04-21 14:10:39 -0500

    VoxelBVH Implementation
    Signed-off-by: Andrew Reidmeyer <areidmeyer@nvidia.com>

--- test/main.cpp
+++ test/main.cpp
@@ -18,6 +18,7 @@
 
 #include <cstdarg>
 
+#include <cstdint>
 #include <slang.h>
 
 #define SLANG_PRELUDE_NAMESPACE CPPPrelude
@@ -191,8 +192,13 @@ void test_image_2d(pnanovdb_editor_t* editor,
     printf("Image2d '%s' added to scene\n", image_name);
 }
 
+void voxelbvh_test();
+
 int main(int argc, char* argv[])
 {
+    voxelbvh_test();
+    return 0;
+
     auto args = argparse::parse<NanoVDBEditorArgs>(argc, argv);
 
 #if TEST_NODE2
